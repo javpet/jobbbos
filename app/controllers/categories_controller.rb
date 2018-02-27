756255e20abfa6ef1316fff75c58ba192cf84b7f
javpet <javorkai.peter@gmail.com>
2018-02-27 21:20:29 +0100

    Added new category form

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,25 @@
 class CategoriesController < ApplicationController
 
   def index
+    @categories = Category.all
   end
 
   def create
+    @category = Category.new(form_params)
+
+    if @category.save
+      redirect_to root_path
+    else
+      render "new"
+    end
   end
 
   def new
+    @category = Category.new
+  end
+
+  def form_params
+    params.require(:category).permit(:title)
   end
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+
+  profanity_filter :title
+
+  validates :title, presence: true, length: { maximum: 20 }
 end
